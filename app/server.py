94220f14d4ff5ed913552279c5454f8da3b56b3e
guzman-raphael <raphael.h.guzman@gmail.com>
2022-11-02 15:27:58 -0500

    Properly scope module import only within request.

--- app/server.py
+++ app/server.py
@@ -1,19 +1,17 @@
 from flask import Flask, request
 import time
-import importlib
-import pipeline
+import imp
 
 app = Flask(__name__)
 
 @app.route("/student")
 def student():
-    importlib.reload(pipeline)
-    schema = pipeline.schema
-    schema.activate(f"{request.args.get('prefix')}_university")
+    pipeline=imp.load_module(f"{request.args.get('prefix')}_pipeline", *imp.find_module('pipeline'))
+    pipeline.schema.activate(f"{request.args.get('prefix')}_university")
     print("--- imported ---", flush=True)
     # do stuff
     time.sleep(5)
-    return dict(schema=str(schema),
+    return dict(schema=str(pipeline.schema),
                 message=pipeline.Student().message(),
                 records=pipeline.Student.fetch(
                     as_dict=True
@@ -22,3 +20,7 @@ def student():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, threaded=True)
+
+# notes:
+# - https://stackoverflow.com/questions/11170949/how-to-make-a-copy-of-a-python-module-at-runtime
+# - validate if memory will continue to grow without being garbage collected...

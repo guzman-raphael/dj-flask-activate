fad20f89f6ee657b3d1f0fab0df6bfaa7c29aea2
guzman-raphael <raphael.h.guzman@gmail.com>
2022-11-03 04:01:45 -0500

    Properly scopes connection to its thread.

--- app/server.py
+++ app/server.py
@@ -3,15 +3,28 @@ import datajoint as dj
 import os
 import time
 import imp
+from functools import wraps
 
 app = Flask(__name__)
 
+def protected_route(function):
+    @wraps(function)
+    def wrapper(**kwargs):
+        try:
+            dj.conn(host=os.getenv("DJ_ROOT_HOST"),
+                    user=request.args.get('user'),
+                    password=request.args.get('password'),
+                    reset=True)
+            return function(**kwargs)
+        except Exception as e:
+            return str(e), 401
+
+    wrapper.__name__ = function.__name__
+    return wrapper
+
 @app.route("/student")
+@protected_route
 def student():
-    dj.conn(host=os.getenv("DJ_ROOT_HOST"),
-            user=request.args.get('user'),
-            password=request.args.get('password'),
-            reset=True)
     pipeline=imp.load_module(f"{request.args.get('prefix')}_pipeline",
                              *imp.find_module('pipeline'))
     pipeline.schema.activate(f"{request.args.get('prefix')}_university")
